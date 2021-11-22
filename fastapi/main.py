a03968cee35637c259bf08135ea5403b1978d9ee
Roan <s.yadollahzadeh@gmail.com>
2021-11-22 09:43:13 +0330

    Update

---
+++ fastapi/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from core.config import settings
+from fastapi.staticfiles import StaticFiles
+from apis.general_pages.route_homepage import general_pages_router
+
+
+def include_router(app):
+    app.include_router(general_pages_router)
+
+
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(app)
+    configure_static(app)
+    return app
+
+
+app = start_application()
+
+# @app.get("/")
+# def hello_api():
+#     return {"msg": "Hello API"}
